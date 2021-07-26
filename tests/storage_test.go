743fe716cbbd9488bca1a6dceb5762c6efecb39a
Xuanwo <github@xuanwo.io>
2021-07-26 13:24:41 +0800

    tests: Add benchmark for read and write (#12)
    * tests: Add benchmark for read and write
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Fix build
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- tests/storage_test.go
+++ tests/storage_test.go
@@ -1,14 +1,11 @@
 package tests
 
 import (
-	"log"
-	"os"
 	"testing"
 
 	tests "github.com/beyondstorage/go-integration-test/v4"
 )
 
 func TestStorage(t *testing.T) {
-	log.SetOutput(os.Stderr)
 	tests.TestStorager(t, setupTest(t))
 }
