7a6599b16645007a31911f71e42416d915268b15
abyss-w <mad.hatter@foxmail.com>
2021-09-26 13:03:34 +0800

    Updates write

--- storage.go
+++ storage.go
@@ -1,7 +1,6 @@
 package memory
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"io"
@@ -182,9 +181,9 @@ func (s *Storage) stat(ctx context.Context, path string, opt pairStorageStat) (o
 }
 
 func (s *Storage) write(ctx context.Context, path string, r io.Reader, size int64, opt pairStorageWrite) (n int64, err error) {
-	if r == nil && size == 0 {
-		r = bytes.NewReader([]byte{})
-	} else if r == nil && size != 0 {
+	// According to GSP-751, we should allow the user to pass in a nil io.Reader.
+	// ref: https://github.com/beyondstorage/go-storage/blob/master/docs/rfcs/751-write-empty-file-behavior.md
+	if r == nil && size != 0 {
 		return 0, fmt.Errorf("reader is nil but size is not nil")
 	}
 
@@ -197,21 +196,21 @@ func (s *Storage) write(ctx context.Context, path string, r io.Reader, size int6
 		r = iowrap.CallbackReader(r, opt.IoCallback)
 	}
 
-	var read int
+	if size == 0 {
+		return size, nil
+	}
+	
+	o.mode = ModeRead
+	o.data = make([]byte, size)
 
-	if size != 0 {
-		o.mode = ModeRead
-		o.data = make([]byte, size)
+	// TODO: we need to add integration tests for this case.
+	read, err := r.Read(o.data)
+	// Update o.length even after read met error.
+	o.data = o.data[:read]
+	o.length = int64(read)
 
-		// TODO: we need to add integration tests for this case.
-		read, err = r.Read(o.data)
-		// Update o.length even after read met error.
-		o.data = o.data[:read]
-		o.length = int64(read)
-
-		if err != nil {
-			return int64(read), err
-		}
+	if err != nil {
+		return int64(read), err
 	}
 
 	return int64(read), nil
